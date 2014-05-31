64c3b63e9bd10ac79b7692098cf44bf3b9b9c35f
hernan224 <hernan224@gmail.com>
2014-05-31 03:01:18 -0300

    Contenido completo y ajustes finos
    Se completaron los textos de todas las secciones.
    Se crearon las secciones que faltaban.
    Se agregaron algunas imagenes.
    Se ajusto la hoja de estilo para mejorar posibles errores en los
    diversos navegadores.

--- js/main.js
+++ js/main.js
@@ -11,17 +11,11 @@ $(document).ready(function(){
     var allPanels = $('.accordion > dd').hide();
     var allTriggers = $('.accordion > dt > a');
 
-   //var accordionAbrir_md = '<i class="fa fa-plus-circle">&nbsp;&nbsp;</i> Ver más';
-   //var accordionCerrar_md = '<i class="fa fa-times-circle">&nbsp;&nbsp;</i> Cerrar';
-   //var accordionAbrir_xs = '<i class="fa fa-plus-circle fa-3x">&nbsp;&nbsp;</i>';
-   //var accordionCerrar_xs = '<i class="fa fa-times-circle fa-3x">&nbsp;&nbsp;</i>';
 
     $(allTriggers).click(function() {
-        //e.preventDefault();
+
         $this = $(this);
         $target =  $this.parent().next();
-        //$btnMd = $this.chidren('.btn_md');
-        //$btnXs = $this.chidren('.btn_xs');
 
         //if (!this.hasClass('protocolo_desactivado')){
             if(!$target.hasClass('active')){
--- js/plugins.js
+++ js/plugins.js
@@ -28,28 +28,88 @@
 (function(a,k,g){a.fn.tinyNav=function(l){var c=a.extend({active:"selected",header:"",indent:"- ",label:""},l);return this.each(function(){g++;var h=a(this),b="tinynav"+g,f=".l_"+b,e=a("<select/>").attr("id",b).addClass("tinynav "+b);if(h.is("ul,ol")){""!==c.header&&e.append(a("<option/>").text(c.header));var d="";h.addClass("l_"+b).find("a").each(function(){d+='<option value="'+a(this).attr("href")+'">';var b;for(b=0;b<a(this).parents("ul, ol").length-1;b++)d+=c.indent;d+=a(this).text()+"</option>"});
     e.append(d);c.header||e.find(":eq("+a(f+" li").index(a(f+" li."+c.active))+")").attr("selected",!0);e.change(function(){k.location.href=a(this).val()});a(f).after(e);c.label&&e.before(a("<label/>").attr("for",b).addClass("tinynav_label "+b+"_label").append(c.label))}})}})(jQuery,this,0);
 
-/** Accordion **/
+/** Alerts **/
 
-/*
-(function($) {
++function ($) {
+    'use strict';
 
-    var allPanels = $('.accordion > dd').hide();
+    // ALERT CLASS DEFINITION
+    // ======================
 
-    $('.accordion > dt > a').click(function(e) {
-        e.preventDefault();
-        $this = $(this);
-        $target =  $this.parent().next();
+    var dismiss = '[data-dismiss="alert"]'
+    var Alert   = function (el) {
+        $(el).on('click', dismiss, this.close)
+    }
 
-        if(!$target.hasClass('active')){
-            allPanels.removeClass('active').slideUp();
-            $target.addClass('active').slideDown();
+    Alert.prototype.close = function (e) {
+        var $this    = $(this)
+        var selector = $this.attr('data-target')
+
+        if (!selector) {
+            selector = $this.attr('href')
+            selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
         }
 
-        return false;
-    });
+        var $parent = $(selector)
 
-})(jQuery);
-*/
+        if (e) e.preventDefault()
+
+        if (!$parent.length) {
+            $parent = $this.hasClass('alert') ? $this : $this.parent()
+        }
+
+        $parent.trigger(e = $.Event('close.bs.alert'))
+
+        if (e.isDefaultPrevented()) return
+
+        $parent.removeClass('in')
+
+        function removeElement() {
+            $parent.trigger('closed.bs.alert').remove()
+        }
+
+        $.support.transition && $parent.hasClass('fade') ?
+            $parent
+                .one($.support.transition.end, removeElement)
+                .emulateTransitionEnd(150) :
+            removeElement()
+    }
+
+
+    // ALERT PLUGIN DEFINITION
+    // =======================
+
+    var old = $.fn.alert
+
+    $.fn.alert = function (option) {
+        return this.each(function () {
+            var $this = $(this)
+            var data  = $this.data('bs.alert')
+
+            if (!data) $this.data('bs.alert', (data = new Alert(this)))
+            if (typeof option == 'string') data[option].call($this)
+        })
+    }
+
+    $.fn.alert.Constructor = Alert
+
+
+    // ALERT NO CONFLICT
+    // =================
+
+    $.fn.alert.noConflict = function () {
+        $.fn.alert = old
+        return this
+    }
+
+
+    // ALERT DATA-API
+    // ==============
+
+    $(document).on('click.bs.alert.data-api', dismiss, Alert.prototype.close)
+
+}(jQuery);
+
 
 
 
